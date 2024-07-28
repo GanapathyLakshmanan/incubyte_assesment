fa96ccd5abad99b4779bc16662f022dfb287e019
Ganapathy Lakshmanan <ganapathyror@gmail.com>
2024-07-28 18:02:03 +0530

    handled any amount of numbers

--- StringCalculator.rb
+++ StringCalculator.rb
@@ -10,3 +10,5 @@ calculator = StringCalculator.new
 puts calculator.add("")
 puts calculator.add("1")
 puts calculator.add("1,5")
+puts calculator.add("1,5,3,4")
+puts calculator.add("10,50,30,400")
