82d2bfc7d2e32378529e1d32a1a714a326a68371
Ganapathy Lakshmanan <ganapathyror@gmail.com>
2024-07-28 18:36:24 +0530

    Added exception to handle multiple negative numbers

--- StringCalculator.rb
+++ StringCalculator.rb
@@ -1,3 +1,5 @@
+class NegativeNumberError < StandardError;
+end
 class StringCalculator
     def add(numbers)
         return 0 if numbers.empty?
@@ -6,13 +8,22 @@ class StringCalculator
         if numbers.start_with?("//")
             delimiter_line, numbers = numbers.split("\n",2)
             delimiter = Regexp.escape(delimiter_line[2..-1])
+            delimiter = /#{delimiter}/
         end 
-        
-    numbers.split(/#{delimiter}/).map(&:to_i).sum 
+    
+    number_list = numbers.split(delimiter).map(&:to_i)
+    negative_numbers=number_list.select { |num| num < 0 }
+    
+    unless negative_numbers.empty?
+        raise NegativeNumberError, "negative numbers not allowed #{negative_numbers.join(',')}"
+    end 
+    
+    number_list.sum
     end 
 end 
 
 calculator = StringCalculator.new 
+begin 
 puts calculator.add("")
 puts calculator.add("1")
 puts calculator.add("1,5")
@@ -20,3 +31,9 @@ puts calculator.add("1,5,3,4")
 puts calculator.add("10,50,30,400")
 puts calculator.add("1\n2,3")
 puts calculator.add("//;\n1;2")
+puts calculator.add("1,-2,3,-4")
+
+
+rescue NegativeNumberError => e 
+    puts e.message
+end 
