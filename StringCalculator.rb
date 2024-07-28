cd6885d05b8e0fd4ab6f1c276efc6801085e7436
Ganapathy Lakshmanan <ganapathyror@gmail.com>
2024-07-28 18:07:01 +0530

    handled new lines between numbers (instead of commas)

--- StringCalculator.rb
+++ StringCalculator.rb
@@ -2,7 +2,7 @@ class StringCalculator
     def add(numbers)
         return 0 if numbers.empty?
         
-    numbers.split(',').map(&:to_i).sum 
+    numbers.split(/,|\n/).map(&:to_i).sum 
     end 
 end 
 
@@ -12,3 +12,4 @@ puts calculator.add("1")
 puts calculator.add("1,5")
 puts calculator.add("1,5,3,4")
 puts calculator.add("10,50,30,400")
+puts calculator.add("1\n2,3")
